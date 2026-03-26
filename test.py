abd84329601e87e8592117e6428d9c68d6d1a169
Sameer Upadhyay <sameer.upadhyay@avantika.edu.in>
2026-03-26 14:54:22 +0530

    Update test.py

--- test.py
+++ test.py
@@ -9,7 +9,9 @@
 T = int(input())
 for i in range(T):
   X = int(input())
-  if X >= 2000:
+  if X >= 2000 and X < 10000:
     print("Yes")
+  elif X >= 10000:
+    print("It's too much")
   else:
     print("No")
